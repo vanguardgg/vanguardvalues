0e33341777b8d4007cb15b91f7c2e25ca795ab04
Quark <72112478+QUARK88@users.noreply.github.com>
2021-04-24 08:06:43 -0400

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,15 +1,834 @@
 questions = [
     {
+        "question": "The leadership of the state belong to the ones who led the revolution.",
+        "effect": {
+            "b1": 1,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Only economic planification can free the workers from exploitation.",
+        "effect": {
+            "b1": 0,
+            "b2": 1,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "We must accept that some nations simply aren't fit for socialism.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Under no pretext shall the workers be disarmed.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 1,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Socialism is an urban matter before all.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 1,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 1,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Traditions of the people must be kept.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 1,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "True socialism can only be lead by members of the working class.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 1,
+        }
+    },
+    {
+        "question": "Without competent leadership, socialism will always fail.",
+        "effect": {
+            "b1": -1,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "A market made up of co-ops is the most efficient form of socialism.",
+        "effect": {
+            "b1": 0,
+            "b2": -1,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "The revolution must be spread worldwide.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": -1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Socialism should never demonstrate violence.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": -1,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "The priority of the revolution should be a land reform.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": -1,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": -1,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Religion is just another way to exploit the working class.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": -1,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Socialism can only come from an educated elite.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": -1,
+        }
+    },
+    {
+        "question": "Party politics bring more stability for the workers than any other system.",
+        "effect": {
+            "b1": 1,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Markets will never produce what the people really need.",
+        "effect": {
+            "b1": 0,
+            "b2": 1,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "The main reason i'm a socialist is to save my nation.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "We should developp a stockpile of weapons for the socialist cause.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 1,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Five to ten year plans are needed in agrarian nations.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 1,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 1,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 1,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Aristocratic or autocratic regimes can never represent the working class.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 1,
+        }
+    },
+    {
+        "question": "An engineer knows more than a demagogue how to rule a country.",
+        "effect": {
+            "b1": -1,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Worker's self management can only exist in a market.",
+        "effect": {
+            "b1": 0,
+            "b2": -1,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Nationalism and patriotism have no place in a marxist movement.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": -1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "War is disastrous as most soldiers that die are part of the working class.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": -1,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Without the farmers to feed the workers, socialism will inevitably fail.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": -1,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": -1,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "We should research technologies for the greater good.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": -1,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Pre revolution working class is not able to rule properly and need bourgeois leaders.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": -1,
+        }
+    },
+    {
         "question": "",
         "effect": {
             "b1": 1,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Market socialism is just another strand of capitalism.",
+        "effect": {
+            "b1": 0,
             "b2": 1,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Globalism is a tool for the rich and powerful.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
             "b3": 1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "We have the duty to declare war on enemies of the working man.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
             "b4": 1,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Factory conditions are much more in need of help than rural conditions.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
             "b5": 1,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
             "b6": 1,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
             "b7": 1,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Without the people to support it, a 'socialist' government is illegitimate.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
             "b8": 1,
+        }
+    },
+    {
+        "question": "Power should be transfered to the best suited, not the most influential.",
+        "effect": {
+            "b1": -1,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Planned economies have failed every time.",
+        "effect": {
+            "b1": 0,
+            "b2": -1,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "There should be one united world socialist republic.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": -1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Socialism should be implemented by reform instead of a revolution.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": -1,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": -1,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": -1,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": -1,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": -1,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 1,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "The means of production can't be fully worker owned if markets exist.",
+        "effect": {
+            "b1": 0,
+            "b2": 1,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 1,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 1,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 1,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 1,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 1,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": -1,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": -1,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": -1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "At the end, a revolution will have caused more harm than good to the workers.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": -1,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": -1,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": -1,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": -1,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": -1,
         }
     }
 ];
